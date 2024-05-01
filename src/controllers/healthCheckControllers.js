48fff4866233d279de0c6d0d0b7bc4f3f3b9c799
Shahidul Deowan <deowanshahidul@gmail.com>
2024-05-02 00:50:59 +0600

    add Health Check Controller for check health

---
+++ src/controllers/healthCheckControllers.js
@@ -0,0 +1,16 @@
+import { logger } from "../config/logger/winston.config.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+
+const healthCheck = async (req, res) => {
+  try {
+    res
+      .status(200)
+      .json(new ApiResponse(200, "OK", "Server is up and running"));
+  } catch (error) {
+    logger.error(`Health check failed! ${error.message}`);
+    throw new ApiError(500, "Health check failed !");
+  }
+};
+
+export { healthCheck };
