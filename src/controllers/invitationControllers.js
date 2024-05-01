69fcca9aa6631dee5a85387ef5f91ed030e2c90c
Shahidul Deowan <deowanshahidul@gmail.com>
2024-05-02 04:09:00 +0600

    add invitation controller

--- src/controllers/invitationControllers.js
+++ src/controllers/invitationControllers.js
@@ -1,3 +1,89 @@
-const verifyInvitation = (req, res) => {};
+import ExcelJS from "exceljs";
+import fse from "fs-extra";
+import { resolve } from "path";
+import { logger } from "../config/logger/winston.config.js";
+import { ApiError, ApiJsonError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
-export { verifyInvitation };
+const textFilePath = resolve(
+  `${process.env.TEXT_FILE_PATH || "03-05-2024-event.txt"}`
+);
+const excelFilePath = resolve(
+  `${process.env.EXCEL_FILE_PATH || "03-05-2024-event.xlsx"}`
+);
+
+const generateAndAppendExcelFile = async (currentDate, message) => {
+  try {
+    let workbook;
+    if (fse.existsSync(excelFilePath)) {
+      workbook = new ExcelJS.Workbook();
+      await workbook.xlsx.readFile(excelFilePath);
+    } else {
+      workbook = new ExcelJS.Workbook();
+      workbook.addWorksheet("Data").columns = [
+        { header: "Date", key: "date", width: 20 },
+        { header: "Name", key: "name", width: 20 },
+      ];
+    }
+
+    const worksheet =
+      workbook.getWorksheet("Data") || workbook.addWorksheet("Data");
+
+    worksheet.addRow({ date: currentDate, name: message });
+
+    await workbook.xlsx.writeFile(excelFilePath);
+  } catch (error) {
+    logger.error(error.message);
+  }
+};
+
+const verifyInvitation = (req, res) => {
+  try {
+    const body = req.body;
+    if (!body?.message) {
+      logger.error("Data required!");
+      res.status(400).json(new ApiJsonError(400, "Data required!"));
+      return;
+    }
+    logger.info(`${body?.message}`);
+    res
+      .status(200)
+      .json(new ApiResponse(200, body, "Invitation verified successfully"));
+  } catch (error) {
+    logger.error(error.message || "Invitation failed");
+    throw new ApiError(
+      error.status || 500,
+      error.message || "Internal Server Error"
+    );
+  }
+};
+
+const watchInvitation = async (req, res) => {
+  try {
+    const body = req.body;
+    if (!body?.message) {
+      logger.error("Data required!");
+      res.status(400).json(new ApiJsonError(400, "Data required!"));
+      return;
+    }
+
+    const currentDate = new Date().toLocaleString();
+    const message = body?.message;
+
+    const appendText = `${currentDate} - ${message}`;
+    fse.appendFileSync(textFilePath, appendText + "\n");
+    await generateAndAppendExcelFile(currentDate, message);
+    logger.info(`${body?.message}`);
+    res
+      .status(200)
+      .json(new ApiResponse(200, body, "Invitation watched successfully"));
+  } catch (error) {
+    logger.error(error.message || "Invitation watched failed");
+    throw new ApiError(
+      error.status || 500,
+      error.message || "Internal Server Error"
+    );
+  }
+};
+
+export { verifyInvitation, watchInvitation };
