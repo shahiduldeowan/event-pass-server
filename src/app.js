8152b0f13da9acd3f3cf25ffe5c53ac5ad219658
Shahidul Deowan <deowanshahidul@gmail.com>
2024-05-02 00:33:43 +0600

    add not found route middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
+import { logger } from "./config/logger/winston.config.js";
+import { ApiError } from "./utils/ApiError.js";
 
 const app = express();
 
@@ -16,6 +18,19 @@ app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  logger.error(err.message);
+  throw new ApiError(err.status || 500, err.message || "Internal Server Error");
 });
 
 export { app };
