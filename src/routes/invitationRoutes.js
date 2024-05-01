4eab95bbea7a5046e24d790ecfb39edf583d0c7f
Shahidul Deowan <deowanshahidul@gmail.com>
2024-05-02 04:08:38 +0600

    add watch route

--- src/routes/invitationRoutes.js
+++ src/routes/invitationRoutes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { verifyInvitation } from "../controllers/invitationControllers.js";
+import {
+  verifyInvitation,
+  watchInvitation,
+} from "../controllers/invitationControllers.js";
 
 const router = Router();
 
 router.route("/verify").post(verifyInvitation);
+router.route("/watch").post(watchInvitation);
 
 export default router;
